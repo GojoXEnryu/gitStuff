9a8b15c577723d290308397789837cfd24dc9155
Арнольд <dan.kovor@gmail.com>
2023-06-19 17:50:49 +0300

    jjj

--- Project1/script.js
+++ Project1/script.js
@@ -1,5 +1,15 @@
 
 function H(){
-    document.getElementById('para').innerText =  'Нажааал кнопку'
-    document.getElementById('firstbutton').innerText =  'Нажааал кнопку'
+    var number, result;
+
+    number = document.getElementById('number').value;
+    if(isNaN(number) || number < 1 || number > 100)
+    {
+        result = 'НЕВЕРНО';
+    }
+    else
+    { 
+        result = 'ВЕРНО';
+    }
+    document.getElementById('firstbutton').textContent = result;
 }            
